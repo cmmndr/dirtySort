466cda1dbd88faa341e5a1160580cbc96c4ea55b
cmmndr <commandr@gmx.net>
2017-07-31 11:12:22 +0200

    removed folder from repository

--- IdeaProjects/javaPractice/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Pi pi = new Pi();
-        double result = pi.calculatePi();
-        System.out.println(result);
-    }
-}
